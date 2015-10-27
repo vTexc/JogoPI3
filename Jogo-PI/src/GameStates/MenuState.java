e998f766363631eec94ed42dd1a945ff6a6266d9
brunocasamassa <brunocasamassa@hotmail.com>
2015-10-27 19:36:56 -0200

    Maromba - Menu

--- Jogo-PI/src/GameStates/MenuState.java
+++ Jogo-PI/src/GameStates/MenuState.java
@@ -2,18 +2,16 @@ package GameStates;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 
 import funcional.Audio;
 
 public class MenuState extends GameState {
 	
 	private int currentChoice = 0;
-	private String[] options = {
-		"Start",
-		"Help",
-		"Quit"
-	};
-	
+
+	private Rectangle[] botao;
+	private static final int NUMBOTAO = 4;
 	private Font titleFont;
 	
 	private Font font;
@@ -23,6 +21,12 @@ public class MenuState extends GameState {
 	public MenuState(GameStateManager gsm) {
 		
 		this.gsm = gsm;
+		
+		botao = new Rectangle[NUMBOTAO];
+		
+		for(int x = 0; x < botao.length; x++) {
+			botao[x] = new Rectangle(950/2 , 650/2 + x*60, 100,50);
+		}
 		
 		try {
 			titleFont = new Font(
@@ -54,14 +58,14 @@ public class MenuState extends GameState {
 		
 		// draw menu options
 		g.setFont(font);
-		for(int i = 0; i < options.length; i++) {
+		for(int i = 0; i < botao.length; i++) {
 			if(i == currentChoice) {
 				g.setColor(Color.WHITE);
 			}
 			else {
 				g.setColor(Color.RED);
 			}
-			g.drawString(options[i], 950/2 - font.getSize(), 650/2 + i * font.getSize());
+			g.draw(botao[i]);
 		}
 		
 	}
@@ -72,10 +76,14 @@ public class MenuState extends GameState {
 			gsm.setState(GameStateManager.PLAY);
 		}
 		if(currentChoice == 1) {
-			// help
+			// options
 		}
+
 		if(currentChoice == 2) {
-			bgMusic.stop();
+			// credit
+		}
+		if(currentChoice == 3) {
+//			bgMusic.stop();
 			System.exit(0);
 		}
 	}
@@ -87,16 +95,30 @@ public class MenuState extends GameState {
 		if(k == KeyEvent.VK_UP) {
 			currentChoice--;
 			if(currentChoice == -1) {
-				currentChoice = options.length - 1;
+				currentChoice = botao.length - 1;
 			}
 		}
 		if(k == KeyEvent.VK_DOWN) {
 			currentChoice++;
-			if(currentChoice == options.length) {
+			if(currentChoice == botao.length) {
 				currentChoice = 0;
 			}
 		}
 	}
 	public void keyReleased(int k) {}
+
+	@Override
+	public void mouseClicked(int k) {
+		if(k == MouseEvent.BUTTON1) {
+			gsm.setState(GameStateManager.PLAY);
+			
+		}
+		if(k == MouseEvent.BUTTON2) {
+			
+		}
+		if(k == MouseEvent.BUTTON3) {
+			
+		}
+	}
 	
 }
